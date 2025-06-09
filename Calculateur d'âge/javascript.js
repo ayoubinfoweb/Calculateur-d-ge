83cc3f28d92b86ebb81cfe54ad70abb9b29ffeab
ayoubinfoweb <ayoubmakon@gmail.com>
2025-06-09 16:27:15 +0100

    Add files via upload

---
+++ Calculateur d'âge/javascript.js
@@ -0,0 +1,10 @@
+function date(){
+
+    var a = document.getElementById('dna').value;
+    var b = document.getElementById('dn').value;
+    var c = parseInt(a)-parseInt(b);
+    alert("عمرك الحالي هو :"+c)
+
+
+
+}
